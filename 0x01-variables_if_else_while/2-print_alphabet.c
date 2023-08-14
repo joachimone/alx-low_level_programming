afaf86f3282e39beba88f3de6a38d651bd99b055
joachimone <joachimayomidevictor@yahoo.com>
2023-08-14 16:43:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printts the alphabetic
+ * Return: Always (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+
+}
