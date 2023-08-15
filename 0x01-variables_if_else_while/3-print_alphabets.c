ad1a7a93dde98cd3118dc950cb1a82b0f2c292d9
joachimone <joachimayomidevictor@yahoo.com>
2023-08-14 17:32:24 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char c;
+	char i;
 
-	char d;
-
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'z') {
-			putchar (d);
-			d++;
-		}
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
 	putchar('\n');
 	return (0);
 }
