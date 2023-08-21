370083994e2f304e38d563f869c791517ccf5838
joachimone <joachimayomidevictor@yahoo.com>
2023-08-21 15:22:23 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: the string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+	while (*str != '\0')
+
+	{
+
+	_putchar(*str++);
+	}
+		_putchar('\n');
+
+}
